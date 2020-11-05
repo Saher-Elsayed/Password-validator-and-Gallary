8bf4b06dd3101ba82d9ba1b36655f768012d297c
Saher Elsayed <saher.suga@gmail.com>
2020-11-04 20:57:29 -0600

    added my 5 faav images to rotates between them

--- Ex2/rotatingPictures.js
+++ Ex2/rotatingPictures.js
@@ -1,10 +1,11 @@
 let images = [
-  "http://i.imgur.com/tI5jq2c.jpg",
-  "http://i.imgur.com/37w80TG.jpg",
-  "http://i.imgur.com/B1MCOtx.jpg",
+  "1.jfif",
+  "2.jfif",
+  "3.jpg",
+  "4.jpg",
+  "5.jfif"
 ];
 
-const url = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=200";
 let target = 0;
 
 const goHome = () => {
@@ -12,11 +13,11 @@ const goHome = () => {
 };
 
 const moveBackwards = () => {
-  target = target === 0 ? 2 : target - 1;
+  target = target === 0 ? 4 : target - 1;
   document.getElementById("myImg").src = images[target];
 };
 
 const moveForward = () => {
-  target = target === 2 ? 0 : target + 1;
+  target = target === 4 ? 0 : target + 1;
   document.getElementById("myImg").src = images[target];
 };
